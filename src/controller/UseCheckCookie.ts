5af13e0e411bf3d28b771b0181f8d0a2a1237cfc
Cresswell <cresswelllew@gmail.com>
2025-03-21 02:14:18 +0800

    removed toast

--- src/controller/UseCheckCookie.ts
+++ src/controller/UseCheckCookie.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
-import toast from "react-hot-toast";
 
 const useCheckCookies  = () => {
     const router = useRouter();
@@ -11,10 +10,6 @@ const useCheckCookies  = () => {
             try {
                 await axios.get("/api/users/check-cookies");
             } catch (error: any) {
-                toast.dismiss();
-
-                toast.error("Login Session Expired\nPlease Login Again");
-
                 router.push("/login");
             }
         };
